3db205c2c5358b522e109c58c745499629c55a85
Martin Gelder <darkspotinthecorner@gmail.com>
2012-01-13 13:54:54 +0100

    DarkTip update

--- darktip/modules/wow.item.equipped.js
+++ darktip/modules/wow.item.equipped.js
@@ -79,6 +79,64 @@ DarkTip.registerModule('wow.item.equipped', {
 			'mainhand': 'mainHand',
 			'offhand' : 'offHand',
 			'ranged'  : 'ranged'
+		},
+		'reforge': {
+			'113': { 'source': 6,  'target': 13 }, // Spiri => Dodge Rating
+			'114': { 'source': 6,  'target': 14 }, // Spirit => Parry Rating
+			'115': { 'source': 6,  'target': 31 }, // Spirit => Hit Rating
+			'116': { 'source': 6,  'target': 32 }, // Spirit => Crit Rating
+			'117': { 'source': 6,  'target': 36 }, // Spirit => Haste Rating
+			'118': { 'source': 6,  'target': 37 }, // Spirit => Expertise Rating
+			'119': { 'source': 6,  'target': 49 }, // Spirit => Mastery
+			'120': { 'source': 13, 'target': 6  }, // Dodge Rating => Spirit
+			'121': { 'source': 13, 'target': 14 }, // Dodge Rating => Parry Rating
+			'122': { 'source': 13, 'target': 31 }, // Dodge Rating => Hit Rating
+			'123': { 'source': 13, 'target': 32 }, // Dodge Rating => Crit Rating
+			'124': { 'source': 13, 'target': 36 }, // Dodge Rating => Haste Rating
+			'125': { 'source': 13, 'target': 37 }, // Dodge Rating => Expertise Rating
+			'126': { 'source': 13, 'target': 49 }, // Dodge Rating => Mastery
+			'127': { 'source': 14, 'target': 6  }, // Parry Rating => Spirit
+			'128': { 'source': 14, 'target': 13 }, // Parry Rating => Dodge Rating
+			'129': { 'source': 14, 'target': 31 }, // Parry Rating => Hit Rating
+			'130': { 'source': 14, 'target': 32 }, // Parry Rating => Crit Rating
+			'131': { 'source': 14, 'target': 36 }, // Parry Rating => Haste Rating
+			'132': { 'source': 14, 'target': 37 }, // Parry Rating => Expertise Rating
+			'133': { 'source': 14, 'target': 49 }, // Parry Rating => Mastery
+			'134': { 'source': 31, 'target': 6  }, // Hit Rating => Spirit
+			'135': { 'source': 31, 'target': 13 }, // Hit Rating => Dodge Rating
+			'136': { 'source': 31, 'target': 14 }, // Hit Rating => Parry Rating
+			'137': { 'source': 31, 'target': 32 }, // Hit Rating => Crit Rating
+			'138': { 'source': 31, 'target': 36 }, // Hit Rating => Haste Rating
+			'139': { 'source': 31, 'target': 37 }, // Hit Rating => Expertise Rating
+			'140': { 'source': 31, 'target': 49 }, // Hit Rating => Mastery
+			'141': { 'source': 32, 'target': 6  }, // Crit Rating => Spirit
+			'142': { 'source': 32, 'target': 13 }, // Crit Rating => Dodge Rating
+			'143': { 'source': 32, 'target': 14 }, // Crit Rating => Parry Rating
+			'144': { 'source': 32, 'target': 31 }, // Crit Rating => Hit Rating
+			'145': { 'source': 32, 'target': 36 }, // Crit Rating => Haste Rating
+			'146': { 'source': 32, 'target': 37 }, // Crit Rating => Expertise Rating
+			'147': { 'source': 32, 'target': 49 }, // Crit Rating => Mastery
+			'148': { 'source': 36, 'target': 6  }, // Haste Rating => Spirit
+			'149': { 'source': 36, 'target': 13 }, // Haste Rating => Dodge Rating
+			'150': { 'source': 36, 'target': 14 }, // Haste Rating => Parry Rating
+			'151': { 'source': 36, 'target': 31 }, // Haste Rating => Hit Rating
+			'152': { 'source': 36, 'target': 32 }, // Haste Rating => Crit Rating
+			'153': { 'source': 36, 'target': 37 }, // Haste Rating => Expertise Rating
+			'154': { 'source': 36, 'target': 49 }, // Haste Rating => Mastery
+			'155': { 'source': 37, 'target': 6  }, // Expertise Rating => Spirit
+			'156': { 'source': 37, 'target': 13 }, // Expertise Rating => Dodge Rating
+			'157': { 'source': 37, 'target': 14 }, // Expertise Rating => Parry Rating
+			'158': { 'source': 37, 'target': 31 }, // Expertise Rating => Hit Rating
+			'159': { 'source': 37, 'target': 32 }, // Expertise Rating => Crit Rating
+			'160': { 'source': 37, 'target': 36 }, // Expertise Rating => Haste Rating
+			'161': { 'source': 37, 'target': 49 }, // Expertise Rating => Mastery
+			'162': { 'source': 49, 'target': 6  }, // Mastery => Spirit
+			'163': { 'source': 49, 'target': 13 }, // Mastery => Dodge Rating
+			'164': { 'source': 49, 'target': 14 }, // Mastery => Parry Rating
+			'165': { 'source': 49, 'target': 31 }, // Mastery => Hit Rating
+			'166': { 'source': 49, 'target': 32 }, // Mastery => Crit Rating
+			'167': { 'source': 49, 'target': 36 }, // Mastery => Haste Rating
+			'168': { 'source': 49, 'target': 37 }  // Mastery => Expertise Rating			
 		}
 	},
 	
@@ -106,6 +164,56 @@ DarkTip.registerModule('wow.item.equipped', {
 		if(typeof state['data']['gem0'] !== 'undefined') state['data']['item']['socketInfo']['sockets'][0]['item'] = state['data']['gem0'];
 		if(typeof state['data']['gem1'] !== 'undefined') state['data']['item']['socketInfo']['sockets'][1]['item'] = state['data']['gem1'];
 		if(typeof state['data']['gem2'] !== 'undefined') state['data']['item']['socketInfo']['sockets'][2]['item'] = state['data']['gem2'];
+		
+		var reforgeid = DarkTip.compareRule(state['data'], ('character.items.'+slot+'.tooltipParams.reforge'));
+		
+		if(reforgeid)
+		{
+			var reforgemap    = DarkTip.map("wow.item.equipped", "maps.reforge", reforgeid);
+			var index_source  = -1;
+			var index_target  = -1;
+			var reforgeamount = 0;
+			
+			for(i = 0; i < state['data']['item']['bonusStats'].length; i++)
+			{
+				if(state['data']['item']['bonusStats'][i]['stat'] == reforgemap['source'])
+				{
+					index_source = i;
+				}
+				if(state['data']['item']['bonusStats'][i]['stat'] == reforgemap['target'])
+				{
+					index_target = i;
+				}
+			}
+			
+			if((index_source >= 0))
+			{
+				reforgeamount = Math.floor(parseInt(state['data']['item']['bonusStats'][index_source]['amount']) * 0.4);
+				
+				state['data']['item']['bonusStats'][index_source]['amount']   = state['data']['item']['bonusStats'][index_source]['amount'] - reforgeamount;
+				state['data']['item']['bonusStats'][index_source]['reforged'] = true;
+				
+				if(index_target >= 0)
+				{
+					state['data']['item']['bonusStats'][index_target]['amount']   = state['data']['item']['bonusStats'][index_target]['amount'] + reforgeamount;
+					state['data']['item']['bonusStats'][index_target]['reforged'] = true;
+				}
+				else
+				{
+					state['data']['item']['bonusStats'].push({
+						'stat'    : reforgemap['target'],
+						'reforged': true,
+						'amount'  : reforgeamount
+					});
+				}
+				
+				state['data']['item']['reforge'] = {
+					'source': reforgemap['source'],
+					'target': reforgemap['target'],
+					'amount': reforgeamount
+				}
+			}
+		}
 		
 		return state['data'];
 	},
@@ -152,6 +260,7 @@ DarkTip.registerModule('wow.item.equipped', {
 							'<% } %>' +
 						'<% } %>' +
 					'</div>' +
+					'<% if(this["item"]["reforge"]) { %><div class="darktip-row highlight-custom"><%= this._loc("reforged") %></div><% } %>' +
 					'<% if(this["item"]["baseArmor"]) { %><div class="darktip-row"><%= this._loc("baseArmor") %></div><% } %>' +
 					'<% if(this["item"]["weaponInfo"]) { %>' +
 						'<div>' +
@@ -162,9 +271,7 @@ DarkTip.registerModule('wow.item.equipped', {
 					'<% } %>' +
 					'<%= this._subLoop("templates.fragments.stat.primary", this["item"]["bonusStats"]) %>' +
 					'<% if(this["item"]["gemInfo"]) { %><div class="darktip-row"><%= this["item"]["gemInfo"]["bonus"]["name"] %></div><% } %>' +
-					
 					'<% if(this["item"]["socketInfo"]) { %><div class="block sockets"><%= this._subLoop("templates.fragments.socket", this["item"]["socketInfo"]["sockets"]) %></div><% } %>' +
-					
 					'<% if(this["item"]["allowableClasses"]) { %><div class="darktip-row"><%= this._loc("allowableClasses") %></div><% } %>' +
 					'<% if(this["item"]["allowableRaces"]) { %><div class="darktip-row"><%= this._loc("allowableRaces") %></div><% } %>' +
 					'<% if(this["item"]["requiredLevel"]) { %><div class="darktip-row"><%= this._loc("requiredLevel") %></div><% } %>' +
@@ -257,22 +364,26 @@ DarkTip.registerModule('wow.item.equipped', {
 		'en_US': {
 			'loading'    : 'Loading item...',
 			'not-found'  : 'Item not found',
-			'transmogged': 'Transmogrified to: <%= this["transmog"]["name"] %>'
+			'transmogged': 'Transmogrified to: <%= this["transmog"]["name"] %>',
+			'reforged'   : 'Reforged (<%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["source"]) %> → <%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["target"]) %>)'
 		},
 		'de_DE': {
 			'loading'    : 'Lade Gegenstand...',
 			'not-found'  : 'Gegenstand nicht gefunden',
-			'transmogged': 'Transmogrifiziert zu: <%= this["transmog"]["name"] %>'
+			'transmogged': 'Transmogrifiziert zu: <%= this["transmog"]["name"] %>',
+			'reforged'   : 'Umgeschmiedet (<%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["source"]) %> → <%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["target"]) %>)'
 		},
 		'fr_FR': {
 			'loading'    : 'Chargement Objets...',
 			'not-found'  : 'Objets introuvable',
-			'transmogged': 'Transmogrifié(e) en : <%= this["transmog"]["name"] %>'
+			'transmogged': 'Transmogrifié(e) en : <%= this["transmog"]["name"] %>',
+			'reforged'   : 'Reforged (<%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["source"]) %> → <%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["target"]) %>)'
 		},
 		'es_ES': {
 			'loading'    : 'Cargando objeto...',
 			'not-found'  : 'Objeto no encontrado',
-			'transmogged': 'Transfigurado a: <%= this["transmog"]["name"] %>'
+			'transmogged': 'Transfigurado a: <%= this["transmog"]["name"] %>',
+			'reforged'   : 'Reforged (<%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["source"]) %> → <%= this["item"]["reforge"]["amount"] %> <%= this._loc("itemStatName." + this["item"]["reforge"]["target"]) %>)'
 		}
 	}
 	
