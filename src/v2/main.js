ff53eb9ad5849f9ec9edac24aea719c299fbf9bd
darkspotinthecorner <darkspotinthecorner@gmail.com>
2014-06-29 23:02:46 +0200

    wip

--- src/v2/main.js
+++ src/v2/main.js
@@ -26,11 +26,80 @@
 		logger.log(message);
 	};
 
+	if (typeof globalScope.dust === 'undefined') {
+		DarkTip.log('DarkTip requires dust.js [http://linkedin.github.io/dustjs/] to operate!');
+		return;
+	}
+
+	/* #################### dust.js helpers #################### */
+	(function(dust){
+		dust.helpers.i18n = (function(){
+			var helper = function(chunk, context, bodies, params) {
+				var newContext = dust.makeBase().push(context).push(dust.helpers.i18n.context());
+				var i18nstring = dust.helpers.tap(params.t, chunk, context);
+				if (i18nstring) {
+					var contextlookup = '_i18n_.' + i18nstring;
+					var localized = newContext.get(contextlookup);
+					if (localized) {
+						var newParams = params;
+						delete newParams.t;
+						dust.loadSource(dust.compile(localized, contextlookup));
+						return chunk.partial(contextlookup, context.push(newParams));
+					}
+					return chunk.write('**' + i18nstring + '**');
+				}
+				return chunk;
+			};
+			helper.context = function(context) {
+				if (typeof context === 'undefined') return { '_i18n_': dust.helpers.i18n._context_ || {} };
+				return dust.helpers.i18n._context_ = context;
+			}
+			return helper;
+		})();
+		dust.helpers.api = function(chunk, context, bodies, params) {
+			var body = bodies.block;
+			var skip = bodies['else'];
+			if (body && params && params.query) {
+				var queryId = dust.helpers.tap(params.query, chunk, context);
+				delete params.query;
+				var rawcallData = DarkTip.getApicallData(queryId);
+				var newContext  = context.push(params);
+				return chunk.map(function(chunk) {
+					dust.renderSource(rawcallData.url, newContext, function(err, apicall) {
+						if (apicall) {
+							DarkTip.callApi(
+								apicall,
+								rawcallData.validationFn,
+								function(data) {
+									return chunk.render(body, newContext.push(data)).end();
+								},
+								function(data) {
+									if (skip) {
+										return chunk.render(skip, newContext.push(data)).end();
+									}
+									return chunk.end();
+								}
+							);
+						} else {
+							dust.log('queryId "' + queryId + '" could not be resolved by DarkTip');
+							return chunk.render(skip, newContext);
+						}
+					});
+				});
+			} else {
+				dust.log('No "query" parameter given for @api helper');
+			}
+			return chunk;
+		};
+	})(globalScope.dust);
+	/* ######################################################### */
+
 	DarkTip.jsonp = (function() {
 		var callbackId = 0;
-		var jsonp = function(url, successFn, errorFn, timeout) {
-			var callBackName = "_callback" + callbackId++;
-			var queryString  = "?callback=DarkTip.jsonp." + callBackName;
+		var jsonp = function(url, validationFn, successFn, errorFn, remoteCallbackParam, timeout) {
+			remoteCallbackParam = remoteCallbackParam || 'callback';
+			var callBackName    = '_callback' + callbackId++;
+			var queryString     = '?' + remoteCallbackParam + '=DarkTip.jsonp.' + callBackName;
 			// setup the callback
 			jsonp[callBackName] = function(data) {
 				delete jsonp[callBackName];
@@ -40,8 +109,7 @@
 						errorFn(data.error);
 					}
 				} else {
-					// hard coded, needs pluginable verification
-					if (data.meta.status == 200)
+					if (validationFn(data))
 					{
 						successFn(data);
 					} else {
@@ -50,21 +118,21 @@
 				}
 			};
 
-			var scr = document.createElement("script");
-			scr.type = "text/javascript";
+			var scr = document.createElement('script');
+			scr.type = 'text/javascript';
 			scr.src = url + queryString;
-			var head = document.getElementsByTagName("head")[0];
+			var head = document.getElementsByTagName('head')[0];
 			head.insertBefore(scr, head.firstChild);
 
 			timeout = timeout || 5000;
 			window.setTimeout(function() {
-				if (typeof jsonp[callBackName] == "function") {
+				if (typeof jsonp[callBackName] == 'function') {
 					jsonp[callBackName] = function(data) {
 						delete jsonp[callBackName];
 					};
-					errorFn({ code: 408, message: "Request Timeout", callback: callBackName });
+					errorFn({ code: 408, message: 'Request Timeout', callback: callBackName });
 					window.setTimeout(function() {
-						if (typeof jsonp[callBackName] == "function") {
+						if (typeof jsonp[callBackName] == 'function') {
 							delete jsonp[callBackName];
 						};
 					}, 60000);
@@ -74,22 +142,26 @@
 		return jsonp;
 	})();
 
-	DarkTip.getApicall = function(apicallId) {
-		console.log({'apicallId': apicallId});
+	DarkTip.getApicallData = function(apicallId) {
+		console.log({'DarkTip.getApicall': apicallId});
 
 		if (apicallId == 'github.user') {
-			return '//api.github.com/users/{username}';
+			return {
+				url: '//api.github.com/users/{username}',
+				validationFn: function(data) { return (data.meta.status == 200); }
+			};
 		}
 		if (apicallId == 'github.user.repos') {
-			return '//api.github.com/users/{username}/repos';
+			return {
+				url: '//api.github.com/users/{username}/repos',
+				validationFn: function(data) { return (data.meta.status == 200); }
+			};
 		}
 	};
 
-	DarkTip.callApi = function(url, successFn, errorFn) {
-		return DarkTip.jsonp(url, successFn, errorFn);
+	DarkTip.callApi = function(url, validationFn, successFn, errorFn) {
+		return DarkTip.jsonp(url, validationFn, successFn, errorFn);
 	};
-
-
 
 
 
